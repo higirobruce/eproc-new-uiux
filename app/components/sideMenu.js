e91cb2f95c02e0f328d924ea3a699338a34ce3a9
Bruce Higiro <higirobru@gmail.com>
2024-01-05 08:52:01 +0200

    Update sideMenu.js

--- app/components/sideMenu.js
+++ app/components/sideMenu.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import {
   CopyOutlined,
@@ -13,25 +13,26 @@ import {
   UserOutlined,
 } from "@ant-design/icons";
 import { Menu } from "antd";
-import {useRouter, usePathname} from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { useRouter as nextRouter } from "next/router";
 import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
 
 const SideMenu = ({ setScreen, screen, user }) => {
   let pathName = usePathname();
-  let router = useRouter()
+  let router = useRouter();
   const [current, setCurrent] = useState(pathName.substring(1));
   const [items, setItems] = useState([]);
+  const [itemsSet, setItemsSet] = useState(false);
   useEffect(() => {}, [screen]);
-  
-  let parts = pathName.split('/')
-  if(parts.length>=3){
-    pathName = `/${parts[1]}/${parts[2]}`
+
+  let parts = pathName.split("/");
+  if (parts.length >= 3) {
+    pathName = `/${parts[1]}/${parts[2]}`;
   }
-  useEffect(()=>{
-    setCurrent(pathName.substring(1))
-  },[pathName])
+  useEffect(() => {
+    setCurrent(pathName.substring(1));
+  }, [pathName]);
 
   useEffect(() => {
     let _items = [];
@@ -41,7 +42,14 @@ const SideMenu = ({ setScreen, screen, user }) => {
         _items.push({
           label: "Dashboard",
           key: "system/dashboard",
-          icon: <PieChartOutlined size={28} className={`text-[${current == `system/dashboard` ? `#1677FF` : `#95A1B3`}]`} />,
+          icon: (
+            <PieChartOutlined
+              size={28}
+              className={`text-[${
+                current == `system/dashboard` ? `#1677FF` : `#95A1B3`
+              }]`}
+            />
+          ),
         });
       }
 
@@ -49,7 +57,14 @@ const SideMenu = ({ setScreen, screen, user }) => {
         _items.push({
           label: "Purchase Requests",
           key: "system/requests",
-          icon: <SolutionOutlined size={28} className={`text-[${current == `system/requests` ? `#1677FF` : `#95A1B3`}]`} />,
+          icon: (
+            <SolutionOutlined
+              size={28}
+              className={`text-[${
+                current == `system/requests` ? `#1677FF` : `#95A1B3`
+              }]`}
+            />
+          ),
         });
       }
 
@@ -57,7 +72,14 @@ const SideMenu = ({ setScreen, screen, user }) => {
         _items.push({
           label: "Tenders",
           key: "system/tenders",
-          icon: <MessageOutlined size={28} className={`text-[${current == `system/tenders` ? `#1677FF` : `#95A1B3`}]`} />,
+          icon: (
+            <MessageOutlined
+              size={28}
+              className={`text-[${
+                current == `system/tenders` ? `#1677FF` : `#95A1B3`
+              }]`}
+            />
+          ),
         });
       }
 
@@ -65,7 +87,14 @@ const SideMenu = ({ setScreen, screen, user }) => {
         _items.push({
           label: "Contracts",
           key: "system/contracts",
-          icon: <FileDoneOutlined size={28} className={`text-[${current == `system/contracts` ? `#1677FF` : `#95A1B3`}]`} />,
+          icon: (
+            <FileDoneOutlined
+              size={28}
+              className={`text-[${
+                current == `system/contracts` ? `#1677FF` : `#95A1B3`
+              }]`}
+            />
+          ),
         });
       }
 
@@ -73,28 +102,47 @@ const SideMenu = ({ setScreen, screen, user }) => {
         _items.push({
           label: "Purchase Orders",
           key: "system/purchase-orders",
-          icon: <OrderedListOutlined size={28} className={`text-[${current == `system/purchase-orders` ? `#1677FF` : `#95A1B3`}]`} />,
+          icon: (
+            <OrderedListOutlined
+              size={28}
+              className={`text-[${
+                current == `system/purchase-orders` ? `#1677FF` : `#95A1B3`
+              }]`}
+            />
+          ),
         });
       }
 
       _items.push({
         label: "Payment requests",
         key: "system/payment-requests",
-        icon: <DollarOutlined size={28} className={`text-[${current == `system/payment-requests` ? `#1677FF` : `#95A1B3`}]`} />,
+        icon: (
+          <DollarOutlined
+            size={28}
+            className={`text-[${
+              current == `system/payment-requests` ? `#1677FF` : `#95A1B3`
+            }]`}
+          />
+        ),
       });
 
       if (user?.permissions?.canViewPurchaseOrders) {
-        
       }
 
       if (user?.permissions?.canViewVendors) {
         _items.push({
           label: "Vendors",
           key: "system/vendors",
-          icon: <UsergroupAddOutlined size={28} className={`text-[${current == `system/vendors` ? `#1677FF` : `#95A1B3`}]`} />,
+          icon: (
+            <UsergroupAddOutlined
+              size={28}
+              className={`text-[${
+                current == `system/vendors` ? `#1677FF` : `#95A1B3`
+              }]`}
+            />
+          ),
         });
       }
-      
 
       if (user?.permissions?.canViewUsers) {
         _items.push({
@@ -104,13 +152,16 @@ const SideMenu = ({ setScreen, screen, user }) => {
         _items.push({
           label: "Internal Users",
           key: "system/users",
-          icon: <UserOutlined size={28} className={`text-[${current == `system/dashboard` ? `#1677FF` : `#95A1B3`}]`} />,
-        },)
+          icon: (
+            <UserOutlined
+              size={28}
+              className={`text-[${
+                current == `system/dashboard` ? `#1677FF` : `#95A1B3`
+              }]`}
+            />
+          ),
+        });
       }
-
-      
-
-
     } else {
       _items = [
         {
@@ -132,7 +183,7 @@ const SideMenu = ({ setScreen, screen, user }) => {
           label: "My Payment requests",
           key: "system/payment-requests",
           icon: <DollarOutlined />,
-        }
+        },
         // {
         //   key: "logout",
         //   label:"Logout",
@@ -147,6 +198,10 @@ const SideMenu = ({ setScreen, screen, user }) => {
     setItems(_items);
   }, []);
 
+  useEffect(() => {
+    setItemsSet(true);
+  }, [items]);
+
   const logout = () => {
     localStorage.removeItem("user");
     router.push("/auth");
@@ -155,27 +210,45 @@ const SideMenu = ({ setScreen, screen, user }) => {
     if (e.key === "logout") {
       logout();
     } else {
-      router.push(`${e.key}`)
+      router.push(`${e.key}`);
       // setScreen(e.key);
     }
     setCurrent(e.key);
   };
 
-  if(!user) return;
+  if (!user) return;
 
   return (
     <div className="flex flex-col gap-10">
-      {items?.map((item) => {
-        return  (item.label && item.label != 'Internal Users') && (
-          <Link href={item.key} className="no-underline flex justify-between items-center">
-            <div className="flex items-center gap-3">
-              {item.icon}
-              {/* <PieChartOutlined size={28} className="text-[#1677FF]" /> */}
-              <span className={`text-[18px] ${current == item.key ? `text-[#1677FF]` : `text-[#95A1B3]`}`}>{item.label}</span>
-            </div>
-            <div className={`w-1 h-6 rounded ${current == item.key ? `bg-[#1677FF]` : `bg-transparent`}`}></div>
-          </Link>
-      )})}
+      {itemsSet &&
+        items?.map((item) => {
+          return (
+            item.label &&
+            item.label != "Internal Users" && (
+              <Link
+                href={item.key}
+                className="no-underline flex justify-between items-center"
+              >
+                <div className="flex items-center gap-3">
+                  {item.icon}
+                  {/* <PieChartOutlined size={28} className="text-[#1677FF]" /> */}
+                  <span
+                    className={`text-[18px] ${
+                      current == item.key ? `text-[#1677FF]` : `text-[#95A1B3]`
+                    }`}
+                  >
+                    {item.label}
+                  </span>
+                </div>
+                <div
+                  className={`w-1 h-6 rounded ${
+                    current == item.key ? `bg-[#1677FF]` : `bg-transparent`
+                  }`}
+                ></div>
+              </Link>
+            )
+          );
+        })}
     </div>
     // <Menu
     //   onClick={onClick}
