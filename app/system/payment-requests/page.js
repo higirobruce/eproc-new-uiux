80ea7e23b679db3366bca8ed17f300af6de99e9e
Bruce Higiro <higirobru@gmail.com>
2024-04-05 20:42:53 +0200

    fixed

--- app/system/payment-requests/page.js
+++ app/system/payment-requests/page.js
@@ -285,7 +285,7 @@ export default function UserRequests() {
       return {
         id: d?._id,
         "Request Number": d?.number,
-        Description: d?.description?.split("\n").join(" "),
+        Description: '"' + d?.description?.split("\n").join(" ") + '"',
         Title: '"' + d?.title?.split("\n").join(" ") + '"',
         Amount: d?.amount,
         Currency: d?.currency,
